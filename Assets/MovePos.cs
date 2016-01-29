da76af0de07eadf142c2856f985c839183ba01eb
Aleksandar Josic <aleksa_josic@hotmail.com>
2016-01-30 00:18:58 +0100

    lokal

---
+++ Assets/MovePos.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePos : MonoBehaviour {
+
+	private Rigidbody2D rigid;
+	public float brzina;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		rigid.MovePosition (rigid.position + (new Vector2 (x, y) * brzina));
+	
+	}
+}
