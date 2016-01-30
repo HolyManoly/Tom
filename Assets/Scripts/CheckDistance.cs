accb6a9dff3ff29cf54b7648eff0cedbc7138011
End-E94 <radojevandrija@gmail.com>
2016-01-30 03:13:12 +0100

    Dodat pokazivac najblizeg objekta i racunanje pozicije pokazivaca

--- Assets/Scripts/CheckDistance.cs
+++ Assets/Scripts/CheckDistance.cs
@@ -5,6 +5,7 @@ public class CheckDistance : MonoBehaviour {
 	public GameObject prviobjekat;
 	public GameObject drugiobjekat;
 	public GameObject treciobjekat;
+	public GameObject pokazivacnajblizeg;
 
 	// Use this for initialization
 	void Start () {
@@ -13,15 +14,17 @@ public class CheckDistance : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Vector3 se dodeljuje Transform promenljivama
 		Transform pozicijahelikoptera = GameObject.Find("Helic").transform.position;
 		Transform pozicijaobjekta1 = prviobjekat.transform.position;
 		Transform pozicijaobjekta2 = drugiobjekat.transform.position;
 		Transform pozicijaobjekta3 = treciobjekat.transform.position;
-
+		//Racunaju se rastojanja tri objekta pojedinacno
 		float rastojanje1 = Vector3.Distance(pozicijahelikoptera, pozicijaobjekta1);
 		float rastojanje2 = Vector3.Distance(pozicijahelikoptera, pozicijaobjekta2);
 		float rastojanje3 = Vector3.Distance(pozicijahelikoptera, pozicijaobjekta3);
 		float najmanjerastojanje;
+		//Racuna se najmanje od sva tri rasotjanja
 		if (rastojanje1 < rastojanje2) {
 			if (rastojanje1 < rastojanje3) {
 				najmanjerastojanje = rastojanje1;
@@ -37,6 +40,22 @@ public class CheckDistance : MonoBehaviour {
 			else {
 				najmanjerastojanje = rastojanje3;
 			}
+		}
+		//Odredjuje se pozicija pokazivaca na osnovu najmanjeg rastojanja. Dodeljuje se pokazivacu Vector3 na osnovu koorinata najblizeg objekta
+		switch (najmanjerastojanje)
+		{
+		case rastojanje1:
+			pokazivacnajblizeg.transfotm.position = new Vector3(prviobjekat.Vector3.x, 3.0f, prviobjekat.transform.position.z ); 
+			break;
+		case rastojanje2:
+			pokazivacnajblizeg.transfotm.position = new Vector3(drugiobjekat.transform.position.x, 3.0f, drugiobjekat.transform.position.z ); 
+			break;
+		case rastojanje3:
+			pokazivacnajblizeg.transfotm.position = new Vector3(treciobjekat.transform.position.x, 3.0f, treciobjekat.transform.position.z ); 
+			break;
+		default:
+			print ("Greska");
+			break;
 		}
 	}
 }
