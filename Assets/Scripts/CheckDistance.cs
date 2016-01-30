35f682d62c5820c67369df72abd6404565886746
End-E94 <radojevandrija@gmail.com>
2016-01-30 03:50:38 +0100

    Ispravljene greske u CheckDistance

--- Assets/Scripts/CheckDistance.cs
+++ Assets/Scripts/CheckDistance.cs
@@ -15,16 +15,20 @@ public class CheckDistance : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Vector3 se dodeljuje Transform promenljivama
-		Transform pozicijahelikoptera = GameObject.Find("Helic").transform.position;
-		Transform pozicijaobjekta1 = prviobjekat.transform.position;
-		Transform pozicijaobjekta2 = drugiobjekat.transform.position;
-		Transform pozicijaobjekta3 = treciobjekat.transform.position;
+		Transform pozicijahelikoptera;
+		pozicijahelikoptera = GameObject.Find("Helic").transform;
+		Transform pozicijaobjekta1; 
+		pozicijaobjekta1 = prviobjekat.transform;
+		Transform pozicijaobjekta2;
+		pozicijaobjekta2 = drugiobjekat.transform;
+		Transform pozicijaobjekta3;
+		pozicijaobjekta3 = treciobjekat.transform;
 		//Racunaju se rastojanja tri objekta pojedinacno
-		float rastojanje1 = Vector3.Distance(pozicijahelikoptera, pozicijaobjekta1);
-		float rastojanje2 = Vector3.Distance(pozicijahelikoptera, pozicijaobjekta2);
-		float rastojanje3 = Vector3.Distance(pozicijahelikoptera, pozicijaobjekta3);
+		float rastojanje1 = Vector3.Distance(pozicijahelikoptera.position, pozicijaobjekta1.position);
+		float rastojanje2 = Vector3.Distance(pozicijahelikoptera.position, pozicijaobjekta2.position);
+		float rastojanje3 = Vector3.Distance(pozicijahelikoptera.position, pozicijaobjekta3.position);
 		float najmanjerastojanje;
-		//Racuna se najmanje od sva tri rasotjanja
+		//Racuna se najmanje od sva tri rastojanja
 		if (rastojanje1 < rastojanje2) {
 			if (rastojanje1 < rastojanje3) {
 				najmanjerastojanje = rastojanje1;
@@ -42,20 +46,16 @@ public class CheckDistance : MonoBehaviour {
 			}
 		}
 		//Odredjuje se pozicija pokazivaca na osnovu najmanjeg rastojanja. Dodeljuje se pokazivacu Vector3 na osnovu koorinata najblizeg objekta
-		switch (najmanjerastojanje)
-		{
-		case rastojanje1:
-			pokazivacnajblizeg.transfotm.position = new Vector3(prviobjekat.Vector3.x, 3.0f, prviobjekat.transform.position.z ); 
-			break;
-		case rastojanje2:
-			pokazivacnajblizeg.transfotm.position = new Vector3(drugiobjekat.transform.position.x, 3.0f, drugiobjekat.transform.position.z ); 
-			break;
-		case rastojanje3:
-			pokazivacnajblizeg.transfotm.position = new Vector3(treciobjekat.transform.position.x, 3.0f, treciobjekat.transform.position.z ); 
-			break;
-		default:
-			print ("Greska");
-			break;
+		if(najmanjerastojanje == rastojanje1){
+			pokazivacnajblizeg.transform.position = new Vector3(prviobjekat.transform.position.x, 3.0f, prviobjekat.transform.position.z ); 
+		}
+		else {
+			if(najmanjerastojanje == rastojanje2){
+			pokazivacnajblizeg.transform.position = new Vector3(drugiobjekat.transform.position.x, 3.0f, drugiobjekat.transform.position.z ); 
+			}
+			else {
+				pokazivacnajblizeg.transform.position = new Vector3(treciobjekat.transform.position.x, 3.0f, treciobjekat.transform.position.z ); 
+			}
 		}
 	}
 }
