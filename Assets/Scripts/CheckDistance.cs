237ef0b7262d18dd71587b755874733748149709
Aleksandar Josic <aleksa_josic@hotmail.com>
2016-01-30 15:46:41 +0100

    Rotacija GUI-ja, sakrivanje iza zgrade

--- Assets/Scripts/CheckDistance.cs
+++ Assets/Scripts/CheckDistance.cs
@@ -1,33 +1,44 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class CheckDistance : MonoBehaviour {
-	public GameObject prviobjekat;
-	public GameObject drugiobjekat;
-	public GameObject treciobjekat;
-	public GameObject pokazivacnajblizeg;
 
-	// Use this for initialization
-	void Start () {
-	
+	public Transform pozicijaHelikoptera;
+	public GameObject ciljevi;
+	public GameObject pokazivaci;
+	private Transform prviObjekat;	
+	private Transform drugiObjekat;
+	private Transform treciObjekat;
+	public GameObject pokazivacnajblizeg;
+	private float rastojanje1;
+	private float rastojanje2;
+	private float rastojanje3;
+	private float najmanjerastojanje;
+	public RectTransform prviPokazivac;
+	public RectTransform drugiPokazivac;
+	public RectTransform treciPokazivac;
+	public float udaljenostPokazivacaOdCentra;
+	private string tekstGUI;
+	private Vector2 fromVector2;
+	private float ugaoPokazivaca;
+	private Vector3 crossProduct;
+
+	void Start()
+	{
+		fromVector2 = new Vector2 (1f, 0f);
+		prviObjekat = ciljevi.transform.GetChild (0);
+		drugiObjekat = ciljevi.transform.GetChild (1);
+		treciObjekat = ciljevi.transform.GetChild (2);
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
-		//Transform svakog objekta se dodeljuje promenljivama
-		Transform pozicijahelikoptera;
-		pozicijahelikoptera = GameObject.Find("Helic").transform;
-		Transform pozicijaobjekta1; 
-		pozicijaobjekta1 = prviobjekat.transform;
-		Transform pozicijaobjekta2;
-		pozicijaobjekta2 = drugiobjekat.transform;
-		Transform pozicijaobjekta3;
-		pozicijaobjekta3 = treciobjekat.transform;
 		//Racunaju se rastojanja tri objekta pojedinacno
-		float rastojanje1 = Vector3.Distance(pozicijahelikoptera.position, pozicijaobjekta1.position);
-		float rastojanje2 = Vector3.Distance(pozicijahelikoptera.position, pozicijaobjekta2.position);
-		float rastojanje3 = Vector3.Distance(pozicijahelikoptera.position, pozicijaobjekta3.position);
-		float najmanjerastojanje;
+		rastojanje1 = Vector2.Distance(pozicijaHelikoptera.position, prviObjekat.position);
+		rastojanje2 = Vector2.Distance(pozicijaHelikoptera.position, drugiObjekat.position);
+		rastojanje3 = Vector2.Distance(pozicijaHelikoptera.position, treciObjekat.position);
+
 		//Racuna se najmanje od sva tri rastojanja
 		if (rastojanje1 < rastojanje2) {
 			if (rastojanje1 < rastojanje3) {
@@ -45,7 +56,9 @@ public class CheckDistance : MonoBehaviour {
 				najmanjerastojanje = rastojanje3;
 			}
 		}
-		//Odredjuje se pozicija pokazivaca na osnovu najmanjeg rastojanja. Dodeljuje se pokazivacu Vector3 na osnovu koorinata najblizeg objekta
+
+
+	/*	//Odredjuje se pozicija pokazivaca na osnovu najmanjeg rastojanja. Dodeljuje se pokazivacu Vector3 na osnovu koorinata najblizeg objekta
 		if(najmanjerastojanje == rastojanje1){
 			pokazivacnajblizeg.transform.position = new Vector3(prviobjekat.transform.position.x, 3.0f, prviobjekat.transform.position.z ); 
 		}
@@ -56,6 +69,41 @@ public class CheckDistance : MonoBehaviour {
 			else {
 				pokazivacnajblizeg.transform.position = new Vector3(treciobjekat.transform.position.x, 3.0f, treciobjekat.transform.position.z ); 
 			}
-		}
+		}*/
+
+		// Deo za izracunavanje pozicija pokazivaca
+		prviPokazivac.localPosition = ((Vector2)(prviObjekat.position - pozicijaHelikoptera.position).normalized) * udaljenostPokazivacaOdCentra;	
+		tekstGUI = "" + (prviObjekat.position - pozicijaHelikoptera.position).magnitude;
+		prviPokazivac.GetComponentInChildren<Text> ().text = tekstGUI;
+		ugaoPokazivaca = getAngle((prviObjekat.position - pozicijaHelikoptera.position).normalized);
+		prviPokazivac.GetComponent<RectTransform> ().rotation = Quaternion.Euler (0f, 0f, ugaoPokazivaca);
+
+		drugiPokazivac.localPosition = ((Vector2)(drugiObjekat.position - pozicijaHelikoptera.position).normalized) * udaljenostPokazivacaOdCentra;	
+		tekstGUI = "" + (drugiObjekat.position - pozicijaHelikoptera.position).magnitude;
+		drugiPokazivac.GetComponentInChildren<Text> ().text = tekstGUI;
+		ugaoPokazivaca = getAngle((drugiObjekat.position - pozicijaHelikoptera.position).normalized);
+		drugiPokazivac.GetComponent<RectTransform> ().rotation = Quaternion.Euler (0f, 0f, ugaoPokazivaca);
+
+		treciPokazivac.localPosition = ((Vector2)(treciObjekat.position - pozicijaHelikoptera.position).normalized) * udaljenostPokazivacaOdCentra;	
+		tekstGUI = "" + (treciObjekat.position - pozicijaHelikoptera.position).magnitude;
+		treciPokazivac.GetComponentInChildren<Text> ().text = tekstGUI;
+		ugaoPokazivaca = getAngle((treciObjekat.position - pozicijaHelikoptera.position).normalized);
+		treciPokazivac.GetComponent<RectTransform> ().rotation = Quaternion.Euler (0f, 0f, ugaoPokazivaca);
+
 	}
+
+	float getAngle( Vector2 toVector)
+	{
+		ugaoPokazivaca = Vector2.Angle( toVector, fromVector2);
+		crossProduct = Vector3.Cross(toVector, fromVector2);
+
+		if (crossProduct.z > 0)
+			ugaoPokazivaca = 360 - ugaoPokazivaca;
+
+		ugaoPokazivaca += 270f;
+		if (ugaoPokazivaca > 360)
+			ugaoPokazivaca -= 360f;
+		return(ugaoPokazivaca);
+	}
+
 }
--- Assets/Scripts/ZgradaBehaviour.cs
+++ Assets/Scripts/ZgradaBehaviour.cs
@@ -1,15 +1,96 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ZgradaBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private SpriteRenderer[] rendaljkeSpratova;
+	private float vertikala;
+	private float horizontala;
+	private bool inRange;
+	private bool izaZgrade;
+	public Transform senka;
+	public string imePrednjegLejera;
+	public string imeZadnjegLejera;
+	public Sprite spratSolid;
+	public Sprite spratTransparent;
+	public Sprite krovSolid;
+	public Sprite krovTransparent;
+	public Transform prvaTackaLinije;
+	public Transform drugaTackaLinije;
+	private Vector3 vektorPodnozja;
+	private float ugao;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "helikopter") {
+			inRange = true;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Start () {
+		inRange = false;
+		rendaljkeSpratova = GetComponentsInChildren<SpriteRenderer> ();
+		//vertikala = transform.position.y;
+		vertikala = transform.GetChild (0).position.y;
+		horizontala = transform.GetChild (0).position.x;
+		vektorPodnozja = drugaTackaLinije.position - prvaTackaLinije.position;
+	}
+
+	void Update()
+	{
+
+		Debug.DrawLine (prvaTackaLinije.position, drugaTackaLinije.position);
+		if (inRange) {
+			if (izaZgrade) {
+				if (getAngle(senka.position) < 180f){
+					for (int i = 0; i < rendaljkeSpratova.Length; i++) {
+						rendaljkeSpratova [i].sortingLayerName = imeZadnjegLejera;
+						if (i != rendaljkeSpratova.Length - 1)
+							rendaljkeSpratova [i].sprite = spratSolid;
+						else
+							rendaljkeSpratova [i].sprite = krovSolid;
+					}
+					izaZgrade = false;
+				}
+			} else {
+				if (getAngle(senka.position) > 180f){
+					for (int i = 0; i < rendaljkeSpratova.Length; i++) {
+						rendaljkeSpratova [i].sortingLayerName = imePrednjegLejera;
+						if (i != rendaljkeSpratova.Length - 1)
+							rendaljkeSpratova [i].sprite = spratTransparent;
+						else
+							rendaljkeSpratova [i].sprite = krovTransparent;
+					}
+					izaZgrade = true;
+				}
+			}
+		}
+	}
+		
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "helikopter") {
+			inRange = false;
+			izaZgrade = false;
+			for (int i = 0; i < rendaljkeSpratova.Length; i++) {
+				rendaljkeSpratova [i].sortingLayerName = imeZadnjegLejera;
+				if (i != rendaljkeSpratova.Length - 1)
+					rendaljkeSpratova [i].sprite = spratSolid;
+				else
+					rendaljkeSpratova [i].sprite = krovSolid;
+			}
+		}
+	}
+
+	float getAngle( Vector3 tackaHelija)
+	{
+		Vector2 toVector2 = tackaHelija - prvaTackaLinije.position;
+
+		ugao = Vector2.Angle(vektorPodnozja, toVector2);
+		Vector3 cross = Vector3.Cross(vektorPodnozja, toVector2);
+
+		if (cross.z > 0)
+			ugao = 360 - ugao;
+
+		return(ugao);
 	}
 }
