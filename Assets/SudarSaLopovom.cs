7d33eba8010bc39d22bbc50ca5486475bfbc5abc
End-E94 <radojevandrija@gmail.com>
2016-01-31 13:07:19 +0100

    crunch no time to explain

---
+++ Assets/SudarSaLopovom.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SudarSaLopovom : MonoBehaviour {
+	public Sprite lopovuhvacen;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "helikopter") {
+			// dodaj caj u inventar
+			GetComponent<SpriteRenderer>().sprite = lopovuhvacen;
+		}
+		
+	}
+}
